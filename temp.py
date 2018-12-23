563ad0ae34845c113ecec8080a0677bbf282cc62
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2018-12-23 11:16:23 +0800

    添加拟合度计算

--- temp.py
+++ temp.py
@@ -12,7 +12,7 @@ import math
 from scipy.optimize import curve_fit
 
 
-hdulist = fits.open('E:/pytest/fits/spec-55860-B6001_sp01-008.fits/spec-55860-B6001_sp01-008.fits')
+hdulist = fits.open('E:/pytest/fits/spec-55860-B6001_sp01-002.fits/spec-55860-B6001_sp01-002.fits')
 hdulist.info()
 print(hdulist[0].header)
 print(hdulist[0].header['RA'])
@@ -26,10 +26,10 @@ x = hdulist[0].data[2]
 #print(x)
 #plt.plot(x[2582:2587],y[2582:2587])
 a = []
-a = x[2579:2587]
+a = x[2579:2589]
 
 b = []
-b = y[2579:2587]
+b = y[2579:2589]
 
 
 maxfuc = max(b)
@@ -44,9 +44,9 @@ def gaussian(x,*param):
 '''
 
 #popt,pcov = curve_fit(gaussian,x,y,p0=[195,6711,8,8,6713,4])
-popt,pcov = curve_fit(gaussian,a,b,p0=[maxfuc,6705,4])
+popt,pcov = curve_fit(gaussian,a,b,p0=[maxfuc,6707,2])
 #help(curve_fit)
-
+modc = gaussian(a,*popt)
 print (popt)
 #print (pcov)
 fig = plt.figure()
@@ -54,11 +54,11 @@ ax0 = fig.add_subplot(3,1,1)
 ax0.plot(a,b,'b',label='data')
 
 ax1 = fig.add_subplot(3,1,2) 
-ax1.plot(a,gaussian(a,*popt),'r',label='fit')
+ax1.plot(a,modc,'r',label='fit')
 
 ax2 = fig.add_subplot(3,1,3) 
 ax2.plot(a,b,'b+:',label='data')
-ax2.plot(a,gaussian(a,*popt),'ro:',label='fit')
+ax2.plot(a,modc,'ro:',label='fit')
 ax2.legend()
 '''
 ax3 = fig.add_subplot(4,1,4) 
@@ -68,5 +68,36 @@ w = popt[2]*math.sqrt(2)
 sigma = w/2
 FWHM = sigma*2.355
 print(w)
+
+'''
+variances = list(map(lambda x,y : (x-y)**2, b, modc))
+#print(variances)
+variance = np.sum(variances)  
+RMSE =  np.sqrt(variance/len(a))
+print(RMSE)
+'''
+
+def R2_fun(y, y_forecast):
+    y_mean=np.mean(y)
+    return 1 - (np.sum((y_forecast - y) ** 2)) / (np.sum((y - y_mean) ** 2))
+
+
+R2 = R2_fun(b,modc)
+print(R2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
